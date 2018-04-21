98f80db54e1f581bb97baf8222ddf059dc084fe1
PoLoMoTo <cbarnwel@stevens.edu>
2018-04-21 15:35:55 -0400

    stuff!

--- main.cpp
+++ main.cpp
@@ -8,4 +8,22 @@ using namespace std;
 
 
 int main(int argc, char const *argv[]){
+
+    if (argc != 9)
+        cout << "get rekt gg no re";
+
+    ifstream file_list, dir_list;
+    int disk_size, block_size;
+    
+    for (int i = 1; i < 9; i+=2){
+        if ((string)argv[i] == "-f"){
+            file_list.open(argv[i+1]);
+        } else if ((string)argv[i] == "-d"){
+            dir_list.open(argv[i+1]);
+        } else if ((string)argv[i] == "-s"){
+            disk_size = stoi(argv[i+1]);
+        } else if ((string)argv[i] == "-b"){
+            block_size = stoi(argv[i+1]);
+        }
+    }
 }
